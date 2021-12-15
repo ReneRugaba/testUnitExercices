7bd02f2ee10570f7c8f153af6f201d19cb290363
ReneRugaba <rugabarj@gmail.com>
2021-12-15 16:06:28 +0100

    end of kata 03

--- 03/test03.js
+++ 03/test03.js
@@ -1,5 +1,14 @@
 
-for (let i = 1; i <= 10; i++) {
-   console.log(i%2!=0?" # # # #":"# # # #");
-    
+/**
+ * return chess array
+ * 
+ * @returns array[char]
+ */
+export function chessboard(){
+   let arraChessBoard = [] ;
+   for (let i = 1; i <= 10; i++) {
+      // console.log(i%2!=0?" # # # #":"# # # #");
+      i%2 != 0 ? arraChessBoard.push(" # # # #") : arraChessBoard.push(" # # # #");
+   }
+   return arraChessBoard;
 }
---
+++ test/03/test03.spec.js
@@ -0,0 +1,33 @@
+import chai from 'chai';
+import { chessboard } from '../../03/test03.js';
+const assert = chai.assert;
+
+
+describe("#chessboard()",()=>{
+    let chess= chessboard();
+    it("should return array", ()=>{
+        assert.isArray(chess,"Not array");
+    })
+
+    it("should return length array", ()=>{
+        assert.strictEqual(chess.length, 10 ,"Count not equal");
+    })
+
+    it("should found same string",()=>{
+        assert.match(chess[0],/# # # #/, "first regex not match");
+        assert.match(chess[1],/ # # # #/,"second regex not  match");
+    })
+
+    it("should is a string in array",()=>{
+        assert.isString(chess[0], "first regex not match"); 
+        assert.isString(chess[1], "first regex not match");
+    })
+
+    it("should find the first element in array",()=>{
+        assert.strictEqual(chess.shift()," # # # #","Not first element");
+    })
+
+    it("should find the last element in array",()=>{
+        assert.strictEqual(chess.pop()," # # # #","Not first element");
+    })
+})
